9671e9298653d25f5991d5b7f42117fc199af03c
huangsheng <2334170767@qq.com>
2022-05-31 17:18:14 +0800

    datatest修改

--- AS/src/main/java/Server/datatest.java
+++ AS/src/main/java/Server/datatest.java
@@ -23,6 +23,7 @@ public class datatest {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("400");
         try{
             // 注册 JDBC 驱动
             Class.forName(JDBC_DRIVER);
